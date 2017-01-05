68330730f6aa9a6198df28c0e589292206dddf99
Bram van der Holst <bram@h-o.nl>
2017-01-05 11:31:32 +0100

    Move files to root

--- registration.php
+++ registration.php
@@ -2,5 +2,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'ProgrammingAtKstark_CatewithimgWidget',
-    __DIR__
+    __DIR__.'/src'
 );
